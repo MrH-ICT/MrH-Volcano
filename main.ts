b894073ebd9041ef975c31627c2301392781a192
Robert Houghton <121752448+MrH-ICT@users.noreply.github.com>
2023-01-01 13:25:13 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,86 @@
-basic.forever(function () {
-	
-})
+// Magma rising through a vent
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+}
+// Erupting volcano
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+}
